15ac27e3c577b78633c7fd6857b3f3efba9bfb38
MSZ98 <51297472+MSZ98@users.noreply.github.com>
2022-06-13 22:51:46 +0200

    Add files via upload

---
+++ examples/Arduino/Blink/Blink.ino
@@ -0,0 +1,15 @@
+#include <pcf8574.h>
+
+PCF8574 ex1(0x20);
+//PCF8574 ex1(PCF8574::combinationToAddress(0));
+
+// LED diode between GND and P1
+
+void setup() {
+	pinMode(ex1, 1, OUTPUT);
+}
+
+void loop() {
+	digitalToggle(ex1, 1);
+	delay(500);
+}
---
+++ examples/Arduino/Read/Read.ino
@@ -0,0 +1,17 @@
+#include <pcf8574.h>
+
+PCF8574 ex1(0x20);
+//PCF8574 ex1(PCF8574::combinationToAddress(0));
+
+// Example read P0 state
+// Button between GND and P0
+
+void setup() {
+	Serial.begin(9600);
+	pinMode(ex1, 0, INPUT_PULLUP);
+}
+
+void loop() {
+	Serial.printf("%d\n", digitalRead(ex1, 0));
+	delay(500);
+}
