56ed3b001f15ac68b8942633e27fc1a2e8bd56d8
MSZ98 <51297472+MSZ98@users.noreply.github.com>
2022-06-13 22:51:09 +0200

    Add files via upload

--- src/pcf8574.h
+++ src/pcf8574.h
@@ -7,31 +7,50 @@
 #ifndef PCF8574_H
 #define PCF8574_H
 
+#include <Arduino.h>
 #include <Wire.h>
+
+#define PCF8574_CLOCK 100000
 
 class PCF8574 {
 
 	private:
 	int address, reg = 255;
+	TwoWire *wire;
 	
 	public:
+	PCF8574(TwoWire &wire, int address) {
+		this->address = address;
+		this->wire = &wire;
+		Wire.begin();
+		Wire.setClock(PCF8574_CLOCK);
+	}
+	PCF8574(int address) {
+		this->address = address;
+		this->wire = &Wire;
+		Wire.begin();
+		Wire.setClock(PCF8574_CLOCK);
+	}
+	#ifdef STICKBREAKER
 	PCF8574(int SDA, int SCL, int address) {
 		this->address = address;
+		wire = &Wire;
 		Wire.begin(SDA, SCL);
-		Wire.setClock(100000);
+		Wire.setClock(PCF8574_CLOCK);
 	}
+	#endif
 
 	int write(int reg) {
-		Wire.beginTransmission(address);
-		Wire.write(reg);
-		int result = Wire.endTransmission();
+		wire->beginTransmission(address);
+		wire->write(reg);
+		int result = wire->endTransmission();
 		if(result == 0) this->reg = reg;
 		return result;
 	}
 
 	int read() {
-		int result = Wire.requestFrom(address, 1);
-		int reg = Wire.read();
+		int result = wire->requestFrom(address, 1);
+		int reg = wire->read();
 		return reg;
 	}
 
